fa30bb890c43ac2b5c03226e17f2d95a97d7f6d4
BrunoOPaese <brunofalcadepaese@gmail.com>
2021-01-11 08:52:18 -0300

    add bootstrap

---
+++ produtos-frontend/src/app/product-interests-form/product-interests-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-interests-form',
+  templateUrl: './product-interests-form.component.html',
+  styleUrls: ['./product-interests-form.component.css']
+})
+export class ProductInterestsFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ produtos-frontend/src/app/product-interests-list/product-interests-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-interests-list',
+  templateUrl: './product-interests-list.component.html',
+  styleUrls: ['./product-interests-list.component.css']
+})
+export class ProductInterestsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
